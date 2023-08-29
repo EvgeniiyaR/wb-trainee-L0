7e2afc8e9a29ee330b37c85b304e2f8d3104feaa
EvgeniiyaR <tulpansik@mail.ru>
2023-08-29 19:13:12 +0300

    feat: add dynamic prices

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 import './validation';
 import {
+  body,
   amountProductsAll,
   products,
   templateAvailableProduct,
@@ -18,7 +19,13 @@ import {
   buttonClosePopupDelivery,
   buttonClosePopupPayment,
   checkboxChoosePayment,
-  buttonTotal } from '../utils/constants';
+  buttonEditDeliveryExtra,
+  buttonEditPaymentExtra,
+  buttonTotal,
+  totalPrice,
+  countAmount,
+  oldTotalPrice,
+  discount } from '../utils/constants';
 
 amountProductsAll.textContent = products.length;
 
@@ -31,6 +38,7 @@ const generateAvailableProduct = (products) => {
     const name = clone.querySelector('.main__name');
     const color = clone.querySelector('.main__attribute_type_color');
     const size = clone.querySelector('.main__attribute_type_size');
+    const sizeMobile = clone.querySelector('.main__attribute_type_size-mobile');
     const stock = clone.querySelector('.main__stock');
     const company = clone.querySelector('.main__company');
     const count = clone.querySelector('.main__count');
@@ -39,10 +47,16 @@ const generateAvailableProduct = (products) => {
     const buttonCountMinus = clone.querySelector('.main__button-count_type_minus');
     const buttonCountPlus = clone.querySelector('.main__button-count_type_plus');
     const newPrice = clone.querySelector('.main__new-price_type_price');
-    const oldPrice = clone.querySelector('.main__old-price');
+    const newPriceMobile = clone.querySelector('.main__new-price_type_mobile');
+    const oldPrice = clone.querySelector('.main__old-price_type_desktop');
+    const oldPriceMobile = clone.querySelector('.main__old-price_type_mobile');
 
     image.src = product.image;
     name.textContent = product.title;
+
+    if (name.textContent.length > 25) {
+      element.classList.add('main__card_type_mobile-two-row');
+    }
 
     if (product.attributes.color !== '' || product.attributes.size !== '') {
       if (product.attributes.color !== '') {
@@ -50,9 +64,12 @@ const generateAvailableProduct = (products) => {
       }
       if (product.attributes.size !== '') {
         size.textContent = `Размер: ${product.attributes.size}`;
+        sizeMobile.textContent = product.attributes.size;
+        sizeMobile.classList.add('main__attribute_type_visibility-mobile');
       }
     } else {
       attributes.style.display = 'none';
+      element.classList.add('main__card_type_mobile-two-row-without-attributes');
     }
 
     stock.textContent = product.stock;
@@ -76,15 +93,34 @@ const generateAvailableProduct = (products) => {
 
     count.textContent = product.count;
 
+    const indexAmount = countAmount.textContent.indexOf('т');
+    const countTotal = Number(countAmount.textContent.slice(0, indexAmount).replace(/\s/g,''));
+    countAmount.textContent = `${(countTotal + product.count).toLocaleString()} товара`;
+
     if (String(product.newPrice).length <= 3) {
       newPrice.classList.add('main__new-price_type_size');
     }
+
     newPrice.textContent = (product.newPrice * product.count).toLocaleString();
+    newPriceMobile.textContent = (product.newPrice * product.count).toLocaleString();
     oldPrice.textContent = `${(product.oldPrice * product.count).toLocaleString()} сом`;
+    oldPriceMobile.textContent = `${(product.oldPrice * product.count).toLocaleString()} сом`;
+
+    const indexOldTotal = oldTotalPrice.textContent.indexOf('сом');
+    const priceOldTotal = Number(oldTotalPrice.textContent.slice(0, indexOldTotal).replace(/\s/g,''));
+    oldTotalPrice.textContent = `${(priceOldTotal + product.oldPrice * product.count).toLocaleString()} сом`;
+
+    const indexTotal = totalPrice.textContent.indexOf('сом');
+    const priceTotal = Number(totalPrice.textContent.slice(0, indexTotal).replace(/\s/g,''));
+    totalPrice.textContent = `${(priceTotal + product.newPrice * product.count).toLocaleString()} сом`;
+
+    const indexDiscount = discount.textContent.indexOf('сом');
+    const discountTotal = Number(discount.textContent.slice(0, indexDiscount).replace(/\s/g,''));
+    discount.textContent = `${(discountTotal + (product.newPrice * product.count - product.oldPrice * product.count)).toLocaleString()} сом`;
 
     cards.append(clone);
   })
-}
+};
 
 generateAvailableProduct(products);
 
@@ -97,6 +133,7 @@ const generateMissingProduct = (products) => {
     const name = clone.querySelector('.main__name');
     const color = clone.querySelector('.main__attribute_type_color');
     const size = clone.querySelector('.main__attribute_type_size');
+    const sizeMobile = clone.querySelector('.main__attribute_type_size-mobile');
     const attributes = clone.querySelector('.main__attributes');
 
     image.src = product.image;
@@ -108,6 +145,8 @@ const generateMissingProduct = (products) => {
       }
       if (product.attributes.size !== '') {
         size.textContent = `Размер: ${product.attributes.size}`;
+        sizeMobile.textContent = product.attributes.size;
+        sizeMobile.classList.add('main__attribute_type_visibility-mobile');
       }
     } else {
       attributes.style.display = 'none';
@@ -115,7 +154,7 @@ const generateMissingProduct = (products) => {
 
     missingCards.append(clone);
   })
-}
+};
 
 generateMissingProduct(products);
 
@@ -132,6 +171,42 @@ const chooseAll = (e) => {
     });
   }
 };
+
+checkboxChooseProducts.forEach((product) => {
+  product.addEventListener('input', (e) => {
+    const target = e.target;
+    const element = target.parentElement.parentElement.parentElement.parentElement;
+    const newPrice = element.querySelector('.main__new-price_type_price');
+    const oldPrice = element.querySelector('.main__old-price');
+    const count = element.querySelector('.main__count').textContent;
+    const indexTotal = totalPrice.textContent.indexOf('сом');
+    const indexOldPrice = oldPrice.textContent.indexOf('сом');
+    const priceTotal = Number(totalPrice.textContent.slice(0, indexTotal).replace(/\s/g,''));
+    const priceNew = Number(newPrice.textContent.replace(/\s/g,''));
+    const priceOld = Number(oldPrice.textContent.slice(0, indexOldPrice).replace(/\s/g,''));
+
+    const indexAmount = countAmount.textContent.indexOf('т');
+    const countTotal = Number(countAmount.textContent.slice(0, indexAmount).replace(/\s/g,''));
+
+    const indexOldTotal = oldTotalPrice.textContent.indexOf('сом');
+    const priceOldTotal = Number(oldTotalPrice.textContent.slice(0, indexOldTotal).replace(/\s/g,''));
+
+    const indexDiscount = discount.textContent.indexOf('сом');
+    const discountTotal = Number(discount.textContent.slice(0, indexDiscount).replace(/\s/g,''));
+
+    if (target.checked === true) {
+      totalPrice.textContent = `${(priceTotal + priceNew).toLocaleString()} сом`;
+      oldTotalPrice.textContent = `${(priceOldTotal + priceOld).toLocaleString()} сом`;
+      countAmount.textContent = `${(countTotal + Number(count)).toLocaleString()} товара`;
+      discount.textContent = `${(discountTotal + (priceNew - priceOld)).toLocaleString()} сом`;
+    } else {
+      totalPrice.textContent = `${(priceTotal - priceNew).toLocaleString()} сом`;
+      oldTotalPrice.textContent = `${(priceOldTotal - priceOld).toLocaleString()} сом`;
+      countAmount.textContent = `${(countTotal - Number(count)).toLocaleString()} товара`;
+      discount.textContent = `${(discountTotal - (priceNew - priceOld)).toLocaleString()} сом`;
+    }
+  })
+});
 
 const hiddenAvailableProducts = () => {
   cards.classList.toggle('main__card_inactive');
@@ -207,22 +282,28 @@ buttonUpMissingProducts.addEventListener('click',hiddenMissingProducts);
 
 const openPopupDelivery = () => {
   popupDelivery.classList.add('popup__opened');
+  body.style.overflow = 'hidden';
 };
 
 const openPopupPayment = () => {
   popupPayment.classList.add('popup__opened');
+  body.style.overflow = 'hidden';
 };
 
 const closePopupDelivery = () => {
   popupDelivery.classList.remove('popup__opened');
+  body.style.overflow = 'visible';
 };
 
 const closePopupPayment = () => {
   popupPayment.classList.remove('popup__opened');
+  body.style.overflow = 'visible';
 };
 
 buttonEditDelivery.addEventListener('click', openPopupDelivery);
+buttonEditDeliveryExtra.addEventListener('click', openPopupDelivery);
 buttonEditPayment.addEventListener('click', openPopupPayment);
+buttonEditPaymentExtra.addEventListener('click', openPopupPayment);
 
 buttonClosePopupDelivery.addEventListener('click', closePopupDelivery);
 buttonClosePopupPayment.addEventListener('click', closePopupPayment);
